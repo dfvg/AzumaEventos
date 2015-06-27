1bb54407e40dbf85cb51cf1d577694e76fb70951
Diego Giuliani <diego.giuliani@gmail.com>
2015-06-27 14:36:06 -0300

    #1
    Versão inicial do projeto

---
+++ resources/views/layout/principal.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>Azuma Kirin - Gestão de Eventos</title>
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta content="Sistema Gerenciador de Eventos da Azuma Kirin" name="description" />
+	<meta content="Logic Arts Agência Digital / www.logicarts.com.br" name="author" />
+
+	<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+	<link href="assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+	<link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
+</head>
+<body class="">
+
+  @yield('header')
+
+
+
+  <!-- BEGIN CORE JS FRAMEWORK-->
+  <script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/jquery-ui/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/boostrapv3/js/bootstrap.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/breakpoints.js" type="text/javascript"></script>
+  <script src="assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/jquery-block-ui/jqueryblockui.js" type="text/javascript"></script>
+  <!-- END CORE JS FRAMEWORK -->
+  <!-- BEGIN PAGE LEVEL JS -->
+  <script src="assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+  <script src="assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script>
+  <!-- END PAGE LEVEL PLUGINS -->
+
+  <!-- BEGIN CORE TEMPLATE JS -->
+  <script src="assets/js/core.js" type="text/javascript"></script>
+  <script src="assets/js/chat.js" type="text/javascript"></script>
+  <script src="assets/js/demo.js" type="text/javascript"></script>
+  <!-- END CORE TEMPLATE JS -->
+
+  <!-- END NEED TO WORK ON -->
+  </body>
+  </html>
---
+++ webarc/layout/principal.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<meta charset="utf-8" />
+<title>Azuma Eventos</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+<meta content="Aplicativo web de gestão de eventos." name="description" />
+<meta content="Logic Arts (www.logicarts.com.br)" name="author" />
+<!-- BEGIN CORE CSS FRAMEWORK -->
+<link href="assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="assets/plugins/boostrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/boostrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+<link href="assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/animate.min.css" rel="stylesheet" type="text/css"/>
+<!-- END CORE CSS FRAMEWORK -->
+<!-- BEGIN CSS TEMPLATE -->
+<link href="assets/css/style.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/responsive.css" rel="stylesheet" type="text/css"/>
+<link href="assets/css/custom-icon-set.css" rel="stylesheet" type="text/css"/>
+<!-- END CSS TEMPLATE -->
+</head>
+<!-- END HEAD -->
+<!-- BEGIN BODY -->
+<body class="error-body no-top">
+<div class="container">
+
+@yield('conteudo')
+
+</div>
+<!-- END CONTAINER -->
+<!-- BEGIN CORE JS FRAMEWORK-->
+<script src="assets/plugins/jquery-1.8.3.min.js" type="text/javascript"></script>
+<script src="assets/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="assets/js/login.js" type="text/javascript"></script>
+<!-- BEGIN CORE TEMPLATE JS -->
+<!-- END CORE TEMPLATE JS -->
+</body>
+</html>
